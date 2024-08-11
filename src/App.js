9e82e2fe509aeef2e16d2510b253341453818fb1
hanzla6103 <135541628+hanzla6103@users.noreply.github.com>
2024-08-11 13:18:02 +0500

    App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to show new update.
+          Edited <code>src/App.js</code> and this is the new update.
         </p>
         <a
           className="App-link"
